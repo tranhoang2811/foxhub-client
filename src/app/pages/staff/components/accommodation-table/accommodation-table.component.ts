21c0fa1b7c1ef50b78f6885e933a79a3b0737530
nguyentiu <tunht21411c@st.uel.edu.vn>
2023-12-12 14:43:23 +0700

    FH-16 | Accommodation table

---
+++ src/app/pages/staff/components/accommodation-table/accommodation-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accommodation-table',
+  templateUrl: './accommodation-table.component.html',
+  styleUrls: ['./accommodation-table.component.css']
+})
+export class AccommodationTableComponent {
+
+}
--- src/app/pages/staff/staff-routing.module.ts
+++ src/app/pages/staff/staff-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccommodationTableComponent } from './components/accommodation-table/accommodation-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "staff/components/accomodationtable",
+  component: AccommodationTableComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
