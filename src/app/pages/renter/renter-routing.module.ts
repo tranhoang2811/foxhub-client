2c5794c0bce23fca9e11f226302ff21818f122bc
Trần Hoàng <trandanghoang2811@gmail.com>
2023-12-12 11:12:28 +0700

    FH-22 | Add routing

--- src/app/pages/renter/renter-routing.module.ts
+++ src/app/pages/renter/renter-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ReservationHistoryComponent } from './reservation-history/reservation-history.component';
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'renter/reservation-history',
     component: ReservationHistoryComponent,
+  },
+  {
+    path: 'renter/sign-up',
+    component: SignUpFormComponent,
   },
 ];
 
