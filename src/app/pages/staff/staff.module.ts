742af67bfd2ca5449987c08e29c126a594c7cd61
Trần Hoàng <trandanghoang2811@gmail.com>
2023-12-12 14:46:32 +0700

    FH-16 | Fix routing

--- src/app/pages/staff/staff.module.ts
+++ src/app/pages/staff/staff.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StaffRoutingModule } from './staff-routing.module';
 import { AccommodationTableComponent } from './components/accommodation-table/accommodation-table.component';
 
 @NgModule({
-  declarations: [
-    AccommodationTableComponent
-  ],
-  imports: [
-    CommonModule,
-  ]
+  declarations: [AccommodationTableComponent],
+  imports: [StaffRoutingModule, CommonModule],
 })
-export class StaffModule { }
+export class StaffModule {}
