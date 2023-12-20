d1a3ba8046a7f6af08f7f469134d1eb8c03d46fe
Trần Hoàng <trandanghoang2811@gmail.com>
2023-12-20 22:16:29 +0700

    FH-33 | Refactor code

--- src/app/pages/renter/listing/listing.component.ts
+++ src/app/pages/renter/listing/listing.component.ts
@@ -3,16 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-listing',
   templateUrl: './listing.component.html',
-  styleUrls: ['./listing.component.css']
+  styleUrls: ['./listing.component.css'],
 })
 export class ListingComponent {
-  n = 12;
-  listings = Array.from({ length: this.n }, (_, index) => ({
+  listings = Array.from({ length: 9 }, (_, index) => ({
     address: 'Thu Duc District - HCM City',
     rate: 5,
     reviews: 42,
     owner: 'Nguyen Van A',
     price: '$15/day',
-    imageUrl: 'assets/mock/search-result-sample.png'
+    imageUrl: 'assets/mock/search-result-sample.png',
   }));
 }
