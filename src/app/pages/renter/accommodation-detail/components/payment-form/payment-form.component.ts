960768a52405bdd0125a25f7e696ebd66b5a4b1c
Trần Hoàng <trandanghoang2811@gmail.com>
2023-12-21 14:27:23 +0700

    FH-11 | Refactor payment form modal

--- src/app/pages/renter/accommodation-detail/components/payment-form/payment-form.component.ts
+++ src/app/pages/renter/accommodation-detail/components/payment-form/payment-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { EditGuestNumberModalComponent } from './components/edit-guest-number-modal/edit-guest-number-modal.component';
 import { EditTimeModalComponent } from './components/edit-time-modal/edit-time-modal.component';
+
 @Component({
   selector: 'app-payment-form',
   templateUrl: './payment-form.component.html',
@@ -12,12 +13,14 @@ export class PaymentFormComponent {
     private modelService: NgbModal,
     private activeModal: NgbActiveModal
   ) {}
+
   public openEditTimeModal(): void {
     const editTimeModal = this.modelService.open(EditTimeModalComponent, {
       centered: true,
       modalDialogClass: 'edit-time-modal',
     });
   }
+
   public openEditGuestNumberModal(): void {
     const editGuestNumberModal = this.modelService.open(
       EditGuestNumberModalComponent,
@@ -27,6 +30,7 @@ export class PaymentFormComponent {
       }
     );
   }
+
   public closeModal(): void {
     this.activeModal.close();
   }
