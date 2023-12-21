b9da06951ac04c19b29a172ba3283e1e52cb217f
Trần Hoàng <trandanghoang2811@gmail.com>
2023-12-21 14:41:26 +0700

    FH-11 | Refactor booking detail section

--- src/app/pages/renter/accommodation-detail/components/booking-detail-section/booking-detail-section.component.ts
+++ src/app/pages/renter/accommodation-detail/components/booking-detail-section/booking-detail-section.component.ts
@@ -7,13 +7,27 @@ import { PaymentFormComponent } from '../payment-form/payment-form.component';
   styleUrls: ['./booking-detail-section.component.css'],
 })
 export class BookingDetailSectionComponent {
-  rooms = ['#Room1', '#Room2', '#Room3', '#Room4', '#Room5'];
-  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  public rooms = [
+    '#Room1',
+    '#Room2',
+    '#Room3',
+    '#Room4',
+    '#Room5',
+    '#Room1',
+    '#Room2',
+    '#Room3',
+    '#Room4',
+    '#Room5',
+  ];
+  public images = [944, 1011, 984].map(
+    (n) => `https://picsum.photos/id/${n}/900/500`
+  );
   public center: google.maps.LatLngAltitude = {
     lat: 10.762622,
     lng: 106.660172,
   } as google.maps.LatLngAltitude;
   public zoom = 12;
+
   constructor(private modelService: NgbModal) {
     navigator.geolocation.getCurrentPosition((position) => {
       this.center = {
