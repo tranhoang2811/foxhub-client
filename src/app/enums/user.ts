058c443e0f2192abd2fbe990053815ad951ad0ec
Trần Hoàng <trandanghoang2811@gmail.com>
2023-12-22 22:42:05 +0700

    FH-43 | Integrate pagination api for accommodaion renter

---
+++ src/app/enums/user.ts
@@ -0,0 +1,11 @@
+export enum EUserRole {
+  RENTER = 'renter',
+  OWNER = 'owner',
+  STAFF = 'staff',
+}
+
+export enum EUserStatus {
+  ACTIVE = 'active',
+  ARCHIVED = 'archived',
+  BANNED = 'banned',
+}
--- src/app/pages/renter/listing/listing.component.ts
+++ src/app/pages/renter/listing/listing.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { IPaginatedAccommodation } from 'src/app/interfaces/api/renter/accommodation';
+import { IPaginationList } from 'src/app/interfaces/common';
+import { AccommodationService } from 'src/app/services/renter/accommodation.service';
 
 @Component({
   selector: 'app-listing',
@@ -6,12 +9,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./listing.component.css'],
 })
 export class ListingComponent {
-  listings = Array.from({ length: 9 }, (_, index) => ({
-    address: 'Thu Duc District - HCM City',
-    rate: 5,
-    reviews: 42,
-    owner: 'Nguyen Van A',
-    price: '$15/day',
-    imageUrl: 'assets/mock/search-result-sample.png',
-  }));
+  public accommodations: IPaginatedAccommodation[] = [];
+  public totalCount: number = 0;
+  public errorMessage: string = '';
+
+  constructor(private accommodationService: AccommodationService) {
+    this.paginateAccommodationList();
+  }
+
+  private paginateAccommodationList() {
+    const filter = {
+      skip: 1,
+      limit: 10,
+    };
+
+    this.accommodationService.paginate(filter).subscribe({
+      next: (response: IPaginationList<IPaginatedAccommodation>) => {
+        this.accommodations = response.list;
+        this.totalCount = response.totalCount;
+      },
+      error: (error) => {
+        this.errorMessage = error.message;
+      },
+    });
+  }
+
+  public getAccommodationCity(latitude: number, longitude: number): string {
+    return `${latitude}, ${longitude}`;
+  }
+
+  public roundRating(rating: number): number {
+    return Math.round(rating * 10) / 10;
+  }
 }
