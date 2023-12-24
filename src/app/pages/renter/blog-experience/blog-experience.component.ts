2fbffe8a3e5426f9c970001f33e55f896ff5c2c2
nguyentiu <tunht21411c@st.uel.edu.vn>
2023-12-24 13:17:13 +0700

    FH-31 | Update blog experience

--- src/app/pages/renter/blog-experience/blog-experience.component.ts
+++ src/app/pages/renter/blog-experience/blog-experience.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-blog-experience',
   templateUrl: './blog-experience.component.html',
-  styleUrls: ['./blog-experience.component.css']
+  styleUrls: ['./blog-experience.component.css'],
 })
 export class BlogExperienceComponent {
   cards = [
