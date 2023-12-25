092478704f71a8fa2ebf1a5eb3182a5c3f76798e
Trần Hoàng <trandanghoang2811@gmail.com>
2023-12-25 17:01:07 +0700

    Update reservation paginate

--- src/app/pages/renter/reservation-history/components/history-list/history-list.component.ts
+++ src/app/pages/renter/reservation-history/components/history-list/history-list.component.ts
@@ -13,18 +13,18 @@ export class HistoryListComponent {
   public pageSize: number = 3;
   public page: number = 1;
   public totalCount: number = 0;
+  public currentTab: string = 'all';
   public errorMessage: string = '';
 
   constructor(private reservationService: ReservationService) {
     this.getReservationList(1);
   }
 
-  public getReservationList(page: number) {
+  public getReservationList(page: number, status: string = 'all') {
     const filter = {
       skip: (page - 1) * this.pageSize,
       limit: this.pageSize,
     };
-    const status = 'all';
     this.reservationService.paginate(filter, status).subscribe({
       next: (response: IPaginationList<IReservation>) => {
         this.reservations = response.list;
@@ -38,6 +38,11 @@ export class HistoryListComponent {
 
   public pageChanged(page: number): void {
     this.page = page;
-    this.getReservationList(page);
+    this.getReservationList(page, this.currentTab);
+  }
+
+  public changeTab(tab: string) {
+    this.currentTab = tab;
+    this.getReservationList(this.page, tab);
   }
 }
